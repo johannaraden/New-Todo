b6cdda5b3bca5e5d23bfc4726da8b1d520680c80
johannaraden <51335476+johannaraden@users.noreply.github.com>
2021-03-18 14:00:34 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Header } from '../src/components/Header'
+import { TodoList } from './components/TodoList'
+import { TodoInput } from './components/TodoInput'
+import { Provider } from 'react-redux'
+import { combineReducers, createStore } from '@reduxjs/toolkit'
+import { todos } from './reducers/reducer'
+import { DeviceSize } from './library/DeviceSizes'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 93%;
+  border-radius: 6px;
+  margin: 5em auto;
+  background-color: lightblue;
+  font-family: 'Helvetica';
+  @media ${DeviceSize.tablet} {
+    width: 80%;
+  }
+  @media ${DeviceSize.laptop} {
+    width: 70%;
+  }
+`
+const Main = styled.div`
+  background-color: white;
+  border-radius: 6px;
+`
+
+const reducer = combineReducers({
+  todos: todos.reducer
+})
+
+// Set up the localstorage
+
+const persistedStateJSON = localStorage.getItem('reduxState')
+let persistedState = {}
+
+if (persistedStateJSON) {
+  persistedState = JSON.parse(persistedStateJSON)
 }
 
-export default App;
+// Creating store 
+
+const store = createStore( 
+  reducer,
+  persistedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+
+// Storing state and subscribing to changes in state
+
+  store.subscribe(() => {
+    localStorage.setItem('reduxState', JSON.stringify(store.getState()));
+  })
+
+export const App = () => {
+  return (
+    <Provider store={store}>
+      <AppContainer>
+        <Header />
+        <Main>
+          <TodoList />
+          <TodoInput />
+        </Main>
+      </AppContainer>
+    </Provider>
+  )
+}
